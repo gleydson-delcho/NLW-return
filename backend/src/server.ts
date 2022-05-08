ee7813e5006d16b2bb4c3797f008831825327b65
gleydson-delcho <gleydson.engenhariacivil@gmail.com>
2022-05-08 18:01:23 -0300

    Modify port address

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
-const port = process.env.SERVER_PORT || 8080;
+const port = process.env.PORT || 3333;
 
 app
   .use(cors({
