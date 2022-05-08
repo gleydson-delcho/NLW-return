4e94a65c637275ba4d19ffc44040e06d94f6b7d2
gleydson-delcho <gleydson.engenhariacivil@gmail.com>
2022-05-08 19:06:04 -0300

    Modify cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,10 +6,7 @@ const app = express();
 const port = process.env.PORT || 3333;
 
 app
-  .use(cors({
-    origin: 'https://nlw-return-production-2d55.up.railway.app/', 
-
-  }))
+  .use(cors())
   .use(express.json())
   .use(routes)
   .listen(port, () => {
